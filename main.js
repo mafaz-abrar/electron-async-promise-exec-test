7b2de369d8450747a39b8345a51585672b880c2d
Mafaz Abrar Jan Chowdhury <mafazabrar@proton.me>
2023-04-28 13:11:48 +1000

    Updated comments.

--- main.js
+++ main.js
@@ -28,6 +28,14 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   ipcMain.handle('volumeSystemTools:getPartitions', (e, ...args) => {
+  
+    // Adds a handler for an invokeable IPC. This handler will be called whenever a renderer
+    // calls ipcRenderer.invoke(channel, ...args).
+
+    // If listener returns a Promise, the eventual result of the promise will be returned as a
+    // reply to the remote caller. Otherwise, the return value of the listener will be used as
+    // the value of the reply.
+
     return volumeSystemTools.getPartitionsAsync(...args);
   })
 
